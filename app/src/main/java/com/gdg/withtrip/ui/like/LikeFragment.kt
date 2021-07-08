a5ace2ec182d2e989b1fcae4c11683bb01e3ea66
Kenneth <wlsrhkd4023@gmail.com>
2021-07-08 16:19:00 +0900

    add like fragment

---
+++ app/src/main/java/com/gdg/withtrip/ui/like/LikeFragment.kt
@@ -0,0 +1,14 @@
+package com.gdg.withtrip.ui.like
+
+import android.os.Bundle
+import com.gdg.withtrip.R
+import com.gdg.withtrip.databinding.FragmentLikeBinding
+import com.solar.universe.binding.UniverseViewFragment
+
+class LikeFragment :
+    UniverseViewFragment<FragmentLikeBinding>(R.layout.fragment_like, FragmentLikeBinding::bind) {
+
+    override fun onViewCreated(bind: FragmentLikeBinding, savedInstanceState: Bundle?) {
+
+    }
+}

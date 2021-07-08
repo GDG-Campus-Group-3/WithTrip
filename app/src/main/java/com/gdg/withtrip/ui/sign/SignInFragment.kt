467807d8c7c2ce5a77e03efaab134e65f5a38512
Kenneth <wlsrhkd4023@gmail.com>
2021-07-08 16:12:15 +0900

    add sign in fragment

---
+++ app/src/main/java/com/gdg/withtrip/ui/sign/SignInFragment.kt
@@ -0,0 +1,16 @@
+package com.gdg.withtrip.ui.sign
+
+import android.os.Bundle
+import com.gdg.withtrip.R
+import com.gdg.withtrip.databinding.FragmentSignInBinding
+import com.solar.universe.binding.UniverseViewFragment
+
+class SignInFragment : UniverseViewFragment<FragmentSignInBinding>(
+    R.layout.fragment_sign_in,
+    FragmentSignInBinding::bind
+) {
+
+    override fun onViewCreated(bind: FragmentSignInBinding, savedInstanceState: Bundle?) {
+
+    }
+}

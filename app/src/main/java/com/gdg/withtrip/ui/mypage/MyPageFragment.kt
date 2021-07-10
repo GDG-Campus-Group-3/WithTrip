d1906fe14130d210e9fc041b4da2835c1a6d42d1
millio <millio@watcha.com>
2021-07-10 14:42:32 +0900

    마이페이지 프레그먼트 추가

---
+++ app/src/main/java/com/gdg/withtrip/ui/mypage/MyPageFragment.kt
@@ -0,0 +1,36 @@
+package com.gdg.withtrip.ui.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gdg.withtrip.databinding.FragmentMypageBinding
+
+class MyPageFragment : Fragment() {
+
+    private var _binding: FragmentMypageBinding? = null
+    private val binding: FragmentMypageBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMypageBinding.inflate(layoutInflater,container,false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+
+}

71115efe80dd64fa9cfd9a05a98f2d985f0f611b
millio <millio@watcha.com>
2021-07-11 01:00:14 +0900

    스플래시 화면

---
+++ app/src/main/java/com/gdg/withtrip/ui/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,20 @@
+package com.gdg.withtrip.ui.onboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gdg.withtrip.databinding.ActivityOnboardingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OnBoardingActivity :AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
